d2ee28787bca2854f5a10649fc47dc4bbb4e9334
rzymek <rzymek@gmail.com>
2014-09-08 20:31:47 +0200

    working solution

--- contact-validator.js
+++ contact-validator.js
@@ -1,5 +1,27 @@
 Dane = new Meteor.Collection('dane');
 
+function normalizeEmails(it) {
+    return [it.email1, it.email2].map(function(email) {
+        return email.trim();
+    }).filter(function(email) {
+        return email.length > 0;
+    });
+}
+function all() {
+    return Dane.find({},{sort:{dziecko:1}});
+}
+function fmtDate(date) {
+    function fmt(c, s) {
+        while (s.toString().length < c) {
+            s = '0' + s;
+        }
+        return s;
+    }
+    if(!date)
+        return '';
+    return date.getFullYear() + "-" + fmt(2, date.getMonth() + 1) + "-" + fmt(2, date.getDate()) + " "
+            + fmt(2, date.getHours()) + ":" + fmt(2, date.getMinutes());
+};
 Router.map(function() {
     this.route('validate', {
         path: '/dane/:_id',
@@ -11,39 +33,109 @@ Router.map(function() {
         },
         onAfterAction: function() {
             Dane.update(this.params._id, {
-                $set: {visited: true}
+                $set: {visited: new Date()}
             });
+        },
+        action: function() {
+            if (this.ready())
+                this.render();
+            else
+                this.render('loading');
         }
     });
     this.route('thanks', {
-        path:'/thanks'
+        path: '/thanks'
     });
     this.route('check', {
-        path: '/output.json',
+        path: '/get/:key.json',
         where: 'server',
         action: function() {
+            if (this.params.key !== Assets.getText('key').trim()) {
+                this.response.writeHead(403, {'Content-Type': 'text/plain'});
+                this.response.end('Invalid key');
+                return;
+            }
             this.response.writeHead(200, {'Content-Type': 'text/javascript'});
-            this.response.end(JSON.stringify(Dane.find().fetch(), null, 2));
+            this.response.end(JSON.stringify(all().fetch(), null, 2));
+        }
+    });
+    this.route('check', {
+        path: '/get/:key.csv',
+        where: 'server',
+        action: function() {
+            if (this.params.key !== Assets.getText('key').trim()) {
+                this.response.writeHead(403, {'Content-Type': 'text/plain'});
+                this.response.end('Invalid key');
+                return;
+            }
+            this.response.writeHead(200, {'Content-Type': 'text/csv'});            
+            this.response.end(all().map(function(it,idx){
+                return (idx+1)+','+it.dziecko+','+it.email1+','+
+                        it.email2+','+it.tel1+','+it.tel2+','+
+                        fmtDate(it.visited)+','+
+                        fmtDate(it.updated);
+            }).join('\n'));
+        }
+    });
+    this.route('mail', {
+        path: '/mail/:key',
+        where: 'server',
+        action: function() {
+            if (this.params.key !== Assets.getText('key').trim()) {
+                this.response.writeHead(403, {'Content-Type': 'text/plain'});
+                this.response.end('Invalid key');
+                return;
+            }
+            var resp = this.response;
+            this.response.writeHead(200, {'Content-Type': 'text/plain'});
+            Dane.find({email1:'rzymek+p1@gmail.com'}).map(function(it) {
+                it.to = normalizeEmails(it);
+                return it;
+            }).filter(function(it){
+                return it.to.length > 0;
+            }).forEach(function(it) {
+                var text = 'Witam \n\
+\n\
+Pod adresem http://grupa1.meteor.com/dane/' + it._id + ' można sprawdzić i poprawić\n\
+maile i numery telefonów, które zbierałem na zebraniu.\n\
+\n\
+Pozdrawiam\n\
+Krzysiek Rzymkowski\n\
+\n\
+PS. Jeżeli na stronie pojawi się komunikat "This site is down", to wystarczy odświerzyć strone (klawisz F5).';
+                var email = {
+                    from: 'rzymek+p1@gmail.com',
+                    to: it.to,
+                    subject: 'Lista mailowa grupy 1',
+                    text: text
+                };
+                Email.send(email);
+                resp.write(it.dziecko+'\n');
+            });
+            this.response.end('Done');
         }
     });
 });
 
 
 if (Meteor.isClient) {
+
     Template.validate.helpers({
         json: function(it) {
             return JSON.stringify(it);
         }
     });
     Template.validate.events({
-        'submit form':function(e){
+        'submit form': function(e) {
             e.preventDefault();
-            var result = $(e.target).serializeArray().reduce(function(a,b){
+            var result = $(e.target).serializeArray().reduce(function(a, b) {
                 a[b.name] = b.value;
                 return a;
-            },{});
+            }, {});
             result.updated = new Date();
-            Dane.update(this._id, result);
+            Dane.update(this._id, {
+                $set: result
+            });
             Router.go('thanks');
         }
     });
@@ -65,6 +157,15 @@ if (Meteor.isServer) {
             }).forEach(function(it) {
                 Dane.insert(it);
             });
+        }
+    });
+    Meteor.publish('dane', function(id) {
+        check(id, String);
+        return Dane.find(id);
+    });
+    Dane.allow({
+        update: function() {
+            return true;
         }
     });
 }
